96c3c0da0435456f46dc4acf61e3490672fcca9d
Mozammal Haque Tonoy <realtonoy@gmail.com>
2024-08-17 14:56:01 +0600

    Dependency Injection through constructor via an XML file.

---
+++ SpringProgram06/src/README.md
@@ -0,0 +1,4 @@
+# Constructor Dependency Injection
+1. Dependencies are injected into class through constructor. 
+2. Constructor DI is less readable.
+3. Constructor DI is less flexible.
---
+++ SpringProgram06/src/in/mh/beans/Student.java
@@ -0,0 +1,26 @@
+package in.mh.beans;
+
+
+public class Student {
+	
+	private String name;
+	private int rollno;
+	private Address address;
+	
+	public Student(String name, int rollno, Address address) {
+		//super();
+		System.out.println("Student object constructor invoked.\n");
+		this.name = name;
+		this.rollno = rollno;
+		this.address = address;
+	}
+
+	
+	public void display() {
+		System.out.println("Name: " + name);
+		System.out.println("Roll no: " + rollno);
+		System.out.println("Address: " + address.toString());
+	}
+	
+}
+

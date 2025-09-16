3cde7b1af4e086a77987e5022bcd331e30126f7b
Mozammal Haque <mozammal.haque@sonaliintellect.com>
2025-09-16 14:33:16 +0600

    Dependency Injection through setter methods via Java Configuration file (Eclipse)

---
+++ SpringProgram07-1/src/in/mh/beans/Student.java
@@ -0,0 +1,40 @@
+package in.mh.beans;
+
+
+public class Student {
+	
+	private String name;
+	private int rollno;
+	private Address address;
+	
+	public Student() {
+		System.out.println("Student object created through constructor.");
+	}
+	
+	public void setName(String name) {
+		
+		System.out.println("setName() method invoked.");
+		this.name = name;
+	}
+
+	public void setRollno(int rollno) {
+		
+		System.out.println("setRollno() method invoked.");
+		this.rollno = rollno;
+	}
+
+	public void setAddress(Address address) {
+		
+		System.out.println("setAddress() method invoked.");
+		this.address = address;
+	}
+
+	public void display() {
+		System.out.println("Name: " + name);
+		System.out.println("Roll no: " + rollno);
+		System.out.println("Address: " + address.toString());
+	}
+	
+	
+}
+
---
+++ SpringProgram07-1/src/in/mh/main/Main.java
@@ -0,0 +1,32 @@
+package in.mh.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import in.mh.beans.Student;
+import in.mh.resources.SpringConfigFile;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
+		
+		System.out.println();
+		
+		Student std1 = (Student) context.getBean(Student.class);
+		std1.display();
+		System.out.println(std1.toString());
+		
+		System.out.println();
+		
+		Student std2 = (Student) context.getBean(Student.class);
+		std2.display();
+		System.out.println(std2.toString());
+	}
+}
+
+/*
+ * We will use AnnotationConfigApplicationContext class in case of Java-based Configuration.
+ * AnnotationConfigApplicationContext is a Spring class for programmatic configuration, using annotations like @Configuration and
+ * @Bean. It initializes the application context, managing beans defined in annotated classes.
+ */
